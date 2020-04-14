295a601995c2aefb35bf3348369ec488386770a4
github username <github email address>
2020-04-14 19:56:07 -0400

    first 4 models basic functions

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :tracks
   resources :playlists
   resources :users
+  resources :add_songs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
