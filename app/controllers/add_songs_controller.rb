5a4e6f7283ed2c89157b974efe3d225a2564676c
vmar13 <60378440+vmar13@users.noreply.github.com>
2020-04-14 19:56:07 -0400

    first 4 models basic functions

--- app/controllers/add_songs_controller.rb
+++ app/controllers/add_songs_controller.rb
@@ -1,8 +1,17 @@
-class AddSongsController < ApplicatonRecord
+class AddSongsController < ApplicationController
 
     def new
+        @add_song = AddSong.new 
     end 
 
     def create
+        add_song = AddSong.create(add_song_params)
+        redirect_to new_add_song_path
+    end 
+
+    private
+
+    def add_song_params
+        params.require(:add_song).permit(:playlist_id, :track_id)
     end 
 end 
