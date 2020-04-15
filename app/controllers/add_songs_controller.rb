3c514f72354929e051e77ab628efec3af4cabe0d
vmar13 <60378440+vmar13@users.noreply.github.com>
2020-04-15 09:54:44 -0400

    added RSpotify and other tweaks

--- app/controllers/add_songs_controller.rb
+++ app/controllers/add_songs_controller.rb
@@ -1,13 +1,45 @@
 class AddSongsController < ApplicationController
 
+    def index
+        if !params[:artist_name].empty?
+          @add_song = RSpotify::Artist.search(params[:artist_name])
+        else
+          redirect_to root_path
+        end
+      end
+      def show
+      end
+
+
+    # def index
+    #     @add_songs = AddSong.all 
+    # end 
+    
     def new
         @add_song = AddSong.new 
     end 
 
     def create
         add_song = AddSong.create(add_song_params)
-        redirect_to new_add_song_path
+        redirect_to add_song_path(add_song)
     end 
+
+
+    def show 
+        @add_song = AddSong.find(params[:id])
+    end
+  
+
+    # def search
+    #     q = params[:q]
+    #     return unless q.present?
+
+    #     require 'net/http'
+    #     uri = URI.parse("https://api.spotify.com/v1/search?" + { q: q, type: 'track' }.to_query)
+    #     json = Net::HTTP.get(uri)
+
+    #     @results = JSON.parse(json)["tracks"]["items"]
+    # end 
 
     private
 
