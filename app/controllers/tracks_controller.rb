45dd7415f37213be626f7768550c75b286bbdb5a
github username <github email address>
2020-04-16 14:24:18 -0400

    created addsongs new form and tracks new form

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,6 +1,21 @@
 class TracksController < ApplicationController
 
+    def new
+        @track = Track.new 
+    end 
+        
+    def create
+        track = Track.create(track_params)
+        
+    end 
+        
     def show
         @track = Track.find(params[:id])
+    end 
+
+    private
+
+    def track_params
+        params.require(:track).permit(:name, :album_id)
     end 
 end
