11d6c421ea5ab23eb100431a6db6a1a09d87b3e3
github username <60378440+vmar13@users.noreply.github.com>
2021-07-06 19:30:10 -0400

    tweaked lograge config

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,12 +1,30 @@
-require 'lograge/sql/extension'
+# require 'lograge/sql/extension'
 
-Rails.application.configure do
-    # Lograge config
-    config.lograge.enabled = true
-    config.lograge.formatter = Lograge::Formatters::Json.new
-    config.colorize_logging = false
+# Rails.application.configure do
+#     # Lograge config
+#     config.lograge.enabled = true
+#     config.lograge.formatter = Lograge::Formatters::Json.new
+#     config.colorize_logging = false
 
-    config.lograge.custom_options = lambda do |event|
-      { :params => event.payload[:params] }
-  end
+#     config.lograge.custom_options = lambda do |event|
+#       { :params => event.payload[:params] }
+#   end
+
+# Lograge config
+config.lograge.enabled = true
+
+# This specifies to log in JSON format
+config.lograge.formatter = Lograge::Formatters::Json.new
+
+## Disables log coloration
+config.colorize_logging = false
+
+# Log to a dedicated file
+config.lograge.logger = ActiveSupport::Logger.new(File.join(Rails.root, 'log', "#{Rails.env}.log"))
+
+# This is useful if you want to log query parameters
+config.lograge.custom_options = lambda do |event|
+    { :ddsource => 'ruby',
+      :params => event.payload[:params].reject { |k| %w(controller action).include? k }
+    }
 end
